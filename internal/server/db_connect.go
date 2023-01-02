b7241cd93b963c55a08a366630cd95d3980c9231
Vladimir Imekov <imekov-vladimir90@yandex.ru>
2023-01-02 18:51:15 +0100

    изменил параметры подключения

--- internal/server/db_connect.go
+++ internal/server/db_connect.go
@@ -2,25 +2,12 @@ package server
 
 import (
 	"database/sql"
-	"fmt"
 	_ "github.com/lib/pq"
-	"net"
-)
-
-const (
-	user     = "postgres"
-	password = "12345678"
-	dbname   = "study_db"
 )
 
 func Connect(DBAddress string) *sql.DB {
-	host, port, err := net.SplitHostPort(DBAddress)
-	if err != nil {
-		panic(err)
-	}
 
-	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-	db, err := sql.Open("postgres", dsn)
+	db, err := sql.Open("postgres", DBAddress)
 	if err != nil {
 		panic(err)
 	}
